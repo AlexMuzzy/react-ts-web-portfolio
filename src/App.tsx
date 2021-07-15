489718c9f5a347387a9507d5a4425c545e5dcff6
AlexMuzzy <alexmuzzy33@gmail.com>
2021-07-15 19:18:29 +0100

    Finished landing page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
+import React, { Component } from "react";
+
+
 import LandingPage from "./LandingPage/landingPage"
 
-const App = () => {
-  return (
-    <LandingPage/>
-  );
+type AppState = { height: number, width: number };
+type AppProps = {}; // There should be no props since this is the 'main' parent component.
+
+class App extends Component<AppProps, AppState>{
+
+  constructor(props: AppProps){
+    super(props);
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+   /**
+   * Events to listen on each resize of browser. Will allow for size of landing page
+   * component to be dynamically resized.
+   * */
+
+    componentDidMount() {
+      this.updateWindowDimensions();
+      window.addEventListener("resize", this.updateWindowDimensions);
+    }
+  
+    componentWillUnmount() {
+      window.removeEventListener("resize", this.updateWindowDimensions);
+    }
+  
+    updateWindowDimensions() {
+      let docHeight: number = window.innerHeight;
+      let docWidth: number = window.innerWidth;
+      console.log(docHeight);
+      this.setState({ width: docWidth, height: docHeight });
+    }
+  
+    // Allows for dynamic resizing of document in React to affect background size.
+
+  render(){
+    return (
+      <LandingPage height={this.state.height}/>
+    );
+  }
 }
 
 export default App;
--- src/LandingPage/landingPage.tsx
+++ src/LandingPage/landingPage.tsx
@@ -1,50 +1,25 @@
 import React, { Component } from "react";
-import { Nav, Navbar, Container, Jumbotron, Row, Col } from "react-bootstrap";
+import {
+  Nav,
+  Navbar,
+  Container,
+  Jumbotron,
+  Row,
+  Col,
+  ListGroup,
+} from "react-bootstrap";
 
 import "./landingPage.css";
 
-type MyProps = {};
-type MyState = { width: number; height: number };
+import background from "./../assets/paula-vermeulen-_f2m3mEkaaU-unsplash.jpg";
+
+type MyProps = { height: number };
+type MyState = {};
 
 class LandingPage extends Component<MyProps, MyState> {
   constructor(props: MyProps) {
     super(props);
-    this.state = { width: 0, height: 0 };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
-
-  /**
-   * Events to listen on each resize of browser. Will allow for size of landing page
-   * component to be dynamically resized.
-   * */
-
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener("resize", this.updateWindowDimensions);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions() {
-    // Update Jumbotron size in landing page using devices height.
-    const landingJumbo: number | undefined =
-      document.getElementById("top-navbar")?.clientHeight;
-    let docHeight: number = window.innerHeight;
-    let docWidth: number = window.innerWidth;
-    if (landingJumbo) {
-      console.debug(
-        "Height of Nav Bar found. negating it from background height."
-      );
-      docHeight = docHeight - landingJumbo * 2;
-    } else {
-      console.debug("Nav bar not found. Has it not rendered yet?");
-    }
-    this.setState({ width: docWidth, height: docHeight });
-  }
-
-  // Allows for dynamic resizing of document in React to affect background size.
 
   render() {
     const jumbotronStyle = {
@@ -56,48 +31,73 @@ class LandingPage extends Component<MyProps, MyState> {
     };
 
     const landingRow = {
-      minHeight: this.state.height / 3,
+      minHeight: this.props.height / 3,
     };
 
     return (
-      <>
-        <Navbar bg="light" variant="light" expand="md" id="top-navbar">
-          <Container>
-            <Navbar.Brand href="#home">Alex Musgrove</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbar-nav" />
-            <Navbar.Collapse id="navbar-nav">
-              <Nav className="ml-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link>About</Nav.Link>
-                <Nav.Link>Services</Nav.Link>
-                <Nav.Link>Contact</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <Container style={{ minHeight: this.state.height }}>
+      <Container
+        fluid
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Container
+          style={{
+            height: this.props.height,
+          }}
+        >
+          {/* <Navbar
+            bg="light"
+            fixed="bottom"
+            variant="light"
+            expand="md"
+            id="top-navbar"
+          >
+            <Container>
+              <Navbar.Brand href="#home">Alex Musgrove</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbar-nav" />
+              <Navbar.Collapse id="navbar-nav">
+                <Nav className="ml-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link>About</Nav.Link>
+                  <Nav.Link>Services</Nav.Link>
+                  <Nav.Link>Contact</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar> */}
+
           <Jumbotron className="clear-colour" style={jumbotronStyle}>
             <Row>
-              <Col md="12" lg="10">
-                <Row style={landingRow}></Row>
+              <Col md="12" lg="8">
+                <Row style={landingRow} className="d-none d-lg-block"></Row>
                 <Row style={landingRow}>
-                  <h1 className="m-auto text-center" style={{ height: "100%" }}>
+                  <h1 className="m-auto text-center btn-shine">
                     Alex Musgrove
                   </h1>
                 </Row>
-                <Row style={landingRow}></Row>
+                <Row style={landingRow} className="d-none d-lg-block"></Row>
               </Col>
-              <Col md="12" lg="2" style={{ height: "100%" }}>
-                <Row style={landingRow}></Row>
-                <Row style={{ borderLeft: "2px solid black", ...landingRow }}>
 
+              <Col md="12" lg="4" style={{ height: "100%" }}>
+                <Row style={landingRow} className="d-none d-lg-block"></Row>
+                <Row style={landingRow}>
+                  <ListGroup variant="flush" style={{ width: "100%" }}>
+                    <ListGroup.Item>Home</ListGroup.Item>
+                    <ListGroup.Item>About</ListGroup.Item>
+                    <ListGroup.Item>Services</ListGroup.Item>
+                    <ListGroup.Item>Contact</ListGroup.Item>
+                  </ListGroup>
                 </Row>
-                <Row style={landingRow}></Row>
+                <Row style={landingRow} className="d-none d-lg-block"></Row>
               </Col>
             </Row>
           </Jumbotron>
         </Container>
-      </>
+      </Container>
     );
   }
 }
