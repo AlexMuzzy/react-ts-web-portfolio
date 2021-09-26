58d3c320ca553a70f9a27ca8ff25a54c20a2c904
AlexMuzzy <alexmuzzy33@gmail.com>
2021-09-27 00:48:38 +0100

    Updated Experience Tabs

--- src/Experience/experience.tsx
+++ src/Experience/experience.tsx
@@ -6,85 +6,153 @@ import lboroLogo from "../assets/lboro-logo.jpg";
 class Experience extends Component {
   render() {
     type experienceType = {
-        workName: string;
-        workImage: string;
-        workRole: string;
-      };
-  
-      const experienceItems: experienceType[] = [
-        {
-          workName: "CGI IT UK",
-          workImage: cgiLogo,
-          workRole: "Junior Software Engineer",
-        },
-        {
-          workName: "Loughborough University",
-          workImage: lboroLogo,
-          workRole: "Research Software Developer",
-        },
-        {
-          workName: "CGI IT UK",
-          workImage: cgiLogo,
-          workRole: "Software Engineering Placement",
-        },
-      ];
-  
-      const ExperienceItem = ({
-        workName,
-        workImage,
-        workRole,
-      }: experienceType) => (
-        <Col md="4">
+      workName: string;
+      workImage: string;
+      workRole: string;
+      workDescription?: string;
+      workTechnologies?: string[];
+    };
+
+    const experienceItems: experienceType[] = [
+      {
+        workName: "CGI IT UK",
+        workImage: cgiLogo,
+        workRole: "Junior Software Engineer",
+        workDescription: `Java Web back-end & 
+                          high performance C++ networking software.`,
+        workTechnologies: [
+          "Java",
+          "C++",
+          "SVN",
+          "Linux Development",
+          "GTest",
+          "JUnit",
+          "Java Servlet",
+          "Network Programming",
+        ],
+      },
+      {
+        workName: "Loughborough University",
+        workImage: lboroLogo,
+        workRole: "Research Software Developer",
+        workDescription: `Development of an image manipulation algorithm in data analysis 
+                          software along with python-based front end client.`,
+        workTechnologies: [
+          "Python",
+          "Computer Vision",
+          "Graphics Programming",
+          "NumPy",
+          "SciPy",
+          "Kivy",
+          "SciKit-Image",
+          "Git",
+          "GitHub",
+        ],
+      },
+      {
+        workName: "CGI IT UK",
+        workImage: cgiLogo,
+        workRole: "Software Engineering Placement",
+        workDescription: `Java web API Devlopment & 
+                          design and development of 
+                          a Python server side automation tool.`,
+        workTechnologies: [
+          "Java",
+          "C++",
+          "Python",
+          "SVN",
+          "Robot Framework",
+          "API Development",
+          "Git",
+          "Linux Development",
+          "Bash",
+          "PowerShell",
+          "Maven",
+          "JUnit",
+        ],
+      },
+    ];
+
+    const ExperienceItem = ({
+      workName,
+      workImage,
+      workRole,
+      workDescription,
+      workTechnologies,
+    }: experienceType) => (
+      <Col md="6" xl="4" style={{ display: "flex" }}>
+        <Row>
           <Card
             style={{
-              borderRadius: "2rem",
-              boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+              borderRadius: "1rem",
+              boxShadow:
+                "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
               border: "none",
-              margin: "1.5rem 0",
+              margin: "1em",
+              width: "100%",
             }}
           >
             <Card.Body>
-              <Card.Title className="h4">{workName}</Card.Title>
-              <Card.Img
-                src={workImage}
-                style={{
-                  backgroundColor: "transparent",
-                }}
-              />
-              <Card.Text>
-                <div className="py-2 text-center lead">{workRole}</div>
-              </Card.Text>
+              <Row>
+                <Col sm="4">
+                  <Card.Text>
+                    <div className="h4 lead">{workName}</div>
+                  </Card.Text>
+                  <Card.Img
+                    src={workImage}
+                    style={{
+                      backgroundColor: "transparent",
+                      maxWidth: "10rem",
+                      margin: "0 auto",
+                      display: "block",
+                    }}
+                  />
+                  <Card.Text>
+                    <div className="py-2 text-center lead">{workRole}</div>
+                  </Card.Text>
+                </Col>
+                <Col sm="8">
+                  <Card.Text style={{ paddingTop: "1.5rem" }}>
+                    {workDescription}
+                  </Card.Text>
+                  {workTechnologies?.map((workTechnology: string) => (
+                    <div
+                      style={{
+                        display: "inline-block",
+                        padding: "0.5rem",
+                        margin: "0.5rem",
+                        backgroundColor: "blue",
+                        color: "white",
+                        borderRadius: "1rem",
+                        fontWeight: "lighter",
+                      }}
+                    >
+                      {workTechnology}
+                    </div>
+                  ))}
+                </Col>
+              </Row>
             </Card.Body>
           </Card>
-        </Col>
-      );
+        </Row>
+      </Col>
+    );
 
     return (
       <Container
         style={{
           marginBottom: "3rem",
-          marginTop: "2rem"
+          marginTop: "2rem",
         }}
+        fluid
       >
         <Row
           style={{
             height: "100%",
             display: "flex",
-            justifyContent: "center",
           }}
         >
-          <h1
-            className="text-center display-3"
-            style={{
-              alignItems: "center",
-              display: "flex",
-              flexWrap: "wrap",
-              justifyContent: "center",
-            }}
-          >
-            Experience
-            <div style={{ color: "blue" }}>.</div>
-          </h1>
+          <h3 style={{ paddingLeft: "1rem" }}>Experience</h3>
         </Row>
         <Row>
           {experienceItems.map((experienceItem: experienceType) => (
@@ -95,6 +163,5 @@ class Experience extends Component {
     );
   }
 }
-
 
 export default Experience;
